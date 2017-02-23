dc71bc181f8a855d6be11e2bc16b1d68a7d01d7b
Hung Tran <hung@0x0.ninja>
2017-02-23 06:58:55 -0600

    update auth api tests

--- test/controllers/api/auth_controller_test.exs
+++ test/controllers/api/auth_controller_test.exs
@@ -6,31 +6,39 @@ defmodule FiftyTwo.Api.AuthControllerTest do
   setup do
     user = User.changeset_registration(%User{}, %{username: "username", password: "password", email: "test@test.localhost"})
     user = Repo.insert!(user)
-    {:ok, user: user}
+    conn = build_conn()
+
+    {:ok, conn: conn, user: user}
   end
 
-  test "returns response with valid credentials" do
-    conn = post build_conn(), api_v1_auth_path(build_conn(), :create), %{"username" => "username", "password" => "password"}
+  test "returns response with valid credentials", %{conn: conn} do
+    conn = conn
+    |> post(api_v1_auth_path(conn, :create), %{"username" => "username", "password" => "password"})
+
     assert json_response(conn, 200)
   end
 
-  test "returns error with invalid credentials" do
-    conn = post build_conn(), api_v1_auth_path(build_conn(), :create), %{"username" => "password", "password" => "username"}
+  test "returns error with invalid credentials", %{conn: conn} do
+    conn = conn
+    |> post(api_v1_auth_path(conn, :create), %{"username" => "password", "password" => "username"})
+
     assert json_response(conn, 401)
   end
 
-  test "returns response with valid token", %{user: user} do
+  test "returns response with valid token", %{conn: conn, user: user} do
     {:ok, jwt, _claims} = Guardian.encode_and_sign(user)
 
-    conn = build_conn()
+    conn = conn
     |> put_req_header("authorization", "Bearer #{jwt}")
-    |> delete api_v1_auth_path(build_conn(), :delete, user)
+    |> delete(api_v1_auth_path(conn, :delete, user))
 
     assert json_response(conn, 200)
   end
 
-  test "returns error with invalid token", %{user: user} do
-    conn = delete build_conn(), api_v1_auth_path(build_conn(), :delete, user)
+  test "returns error with invalid token", %{conn: conn, user: user} do
+    conn = conn
+    |> delete(api_v1_auth_path(conn, :delete, user))
+
     assert json_response(conn, 401)
   end
 end
