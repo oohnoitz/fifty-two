6da0c8c0022b21454006a35e798db9f80dd8b453
Hung Tran <hung@0x0.ninja>
2017-02-25 05:08:13 -0600

    enable Heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,11 @@ use Mix.Config
 # which you typically run after static files are built.
 config :fifty_two, FiftyTwo.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("HOST"), port: System.get_env("PORT")],
+  url: [
+    scheme: System.get_env("URL_SCHEME") || "http",
+    host: System.get_env("URL_HOST"),
+    port: System.get_env("URL_PORT")
+  ],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
@@ -25,44 +29,3 @@ config :fifty_two, FiftyTwo.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: System.get_env("DATABASE_SSL") == "true"
-
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :fifty_two, FiftyTwo.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :fifty_two, FiftyTwo.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
-
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :fifty_two, FiftyTwo.Endpoint, server: true
-#
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
